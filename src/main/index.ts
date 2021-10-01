168a49525b753e83e4b849ced1f2ed659e0b38e9
RASEM0n <negativ12fly@gmail.com>
2021-10-01 15:26:59 +0500

    fix

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,21 +1,12 @@
 import * as path from 'path'
 import { app, BrowserWindow } from 'electron'
 
-debugger
-
 const createWindow = () => {
-    // noinspection JSUnusedLocalSymbols
-    let a = app
-    let p = path
-    let d = __dirname
-
-    debugger
-
     let win: BrowserWindow = new BrowserWindow({
         title: 'Application',
         width: 900,
         height: 800,
-        icon: path.join(__dirname, '../../../resources/icon_256x256.png'),
+        icon: path.join(__dirname, '../resources/icon_256x256.png'),
         webPreferences: {
             preload: path.join(app.getAppPath(), 'preload', 'index.js'),
         },
