b9f027471c45f30e308a28d7d80523aaae5f52ca
RASEM0n <negativ12fly@gmail.com>
2021-10-01 12:59:34 +0500

    progress bar

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,8 @@
 import * as path from 'path'
 import { app, BrowserWindow } from 'electron'
 
+let progressInterval: NodeJS.Timer
+
 const createWindow = () => {
     let win: BrowserWindow = new BrowserWindow({
         title: 'Application',
@@ -18,7 +20,25 @@ const createWindow = () => {
     win.on('closed', () => {
         win = null
     })
+
+    const INCREMENT = 0.03
+    const INTERVAL_DELAY = 100 // ms
+
+    let c = 0
+    progressInterval = setInterval(() => {
+        win.setProgressBar(c)
+
+        if (c < 2) {
+            c += INCREMENT
+        } else {
+            c = -INCREMENT * 5
+        }
+    }, INTERVAL_DELAY)
 }
+
+app.on('before-quit', () => {
+    clearInterval(progressInterval)
+})
 
 app.on('ready', () => {
     createWindow()
